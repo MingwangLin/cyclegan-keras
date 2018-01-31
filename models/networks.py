bcdb22864695cdd8bb2c131c2e9e11f7c4d0b430
林明旺 <linmingwang@linmingwangdeMacBook.local>
2018-01-31 17:55:36 +0800

    add 3 layer Patch-Gan discriminator

--- models/networks.py
+++ models/networks.py
@@ -5,19 +5,28 @@ from keras.layers import *
 from keras_contrib.layers.normalization import InstanceNormalization
 
 
-
-def conv_block(x, filters, size, stride=(2, 2), mode='same', act=True):
+def conv_block(x, filters, size, stride=(2, 2), has_norm_layer=True, is_instance_layer=False,
+               has_activation_layer=True, is_leaky_relu=False):
     x = Conv2D(filters, (size, size), strides=stride, padding=mode)(x)
-    x = InstanceNormalization(axis=3)(x)
-    return Activation('relu')(x) if act else x
+    if has_norm_layer:
+        if not is_instance_layer:
+            x = BatchNormalization(axis=3)(x)
+        else:
+            x = InstanceNormalization(axis=3)(x)
+    if has_activation_layer:
+        if not is_leaky_relu:
+            x = Activation('relu')(x)
+        else:
+            x = LeakyReLU(alpha=0.2)(x)
+    return x
 
 
 def res_block(ip, nf=256):
     x = ZeroPadding2D(padding=(1, 1))(ip)
-    x = conv_block(x, nf, 3, (1, 1), mode='valid')
+    x = conv_block(x, nf, 3, (1, 1))
     Dropout(0.5)
     x = ZeroPadding2D(padding=(1, 1))(x)
-    x = conv_block(x, nf, 3, (1, 1), act=False, mode='valid')
+    x = conv_block(x, nf, 3, (1, 1), has_activation_layer=False)
     return add([x, ip])
 
 
@@ -32,11 +41,11 @@ def up_block(x, filters, size):
 def resnet_generator(arr):
     inp = Input(arr.shape[1:])
     x = ZeroPadding2D(padding=(3, 3))(inp)
-    x = conv_block(x, 64, 7, (1, 1), mode='valid')
+    x = conv_block(x, 64, 7, (1, 1))
     x = ZeroPadding2D(padding=(1, 1))(x)
-    x = conv_block(x, 128, 3, (2, 2), mode='valid')
+    x = conv_block(x, 128, 3, (2, 2))
     x = ZeroPadding2D(padding=(1, 1))(x)
-    x = conv_block(x, 256, 3, (2, 2), mode='valid')
+    x = conv_block(x, 256, 3, (2, 2))
     for i in range(9):
         x = res_block(x)
     x = up_block(x, 128, 3)
@@ -44,4 +53,18 @@ def resnet_generator(arr):
     x = ZeroPadding2D(padding=(3, 3))(x)
     x = Conv2D(3, (7, 7), activation='tanh', strides=(1, 1), padding='valid')(x)
     outp = Lambda(lambda x: (x + 1) * 127.5)(x)
+    return inp, outp
+
+
+# Defines the PatchGAN discriminator
+def NLayerDiscriminator(arr):
+    inp = Input(arr.shape[1:])
+    x = ZeroPadding2D(padding=(1, 1))(inp)
+    x = conv_block(x, 64, 4, (2, 2), has_norm_layer=False, is_leaky_relu=True)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = conv_block(x, 128, 4, (2, 2), is_instance_layer=True, is_leaky_relu=True)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = conv_block(x, 256, 4, (2, 2), is_instance_layer=True, is_leaky_relu=True)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = Conv2D(1, (4, 4), activation='sigmoid', strides=(1, 1), padding='valid')(x)
     return inp, outp
