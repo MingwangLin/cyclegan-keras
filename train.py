3bf70a823ea0b6826d8c8f81ba340ce7fac2fcfb
林明旺 <linmingwang@linmingwangdeMacBook.local>
2018-02-15 17:24:08 +0800

    add train.py

---
+++ train.py
@@ -0,0 +1,44 @@
+import time
+from options.train_options import TrainOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+
+opt = TrainOptions().parse()
+data_loader = CreateDataLoader(opt)
+dataset = data_loader.load_data()
+dataset_size = len(data_loader)
+print('#training images = %d' % dataset_size)
+
+model = create_model(opt)
+total_steps = 0
+
+for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+    epoch_start_time = time.time()
+    epoch_iter = 0
+
+    for i, data in enumerate(dataset):
+        iter_start_time = time.time()
+        total_steps += opt.batchSize
+        epoch_iter += opt.batchSize
+        model.set_input(data)
+        model.optimize_parameters()
+
+        if total_steps % opt.display_freq == 0:
+            save_result = total_steps % opt.update_html_freq == 0
+
+        if total_steps % opt.print_freq == 0:
+            errors = model.get_current_errors()
+            t = (time.time() - iter_start_time) / opt.batchSize
+
+        if total_steps % opt.save_latest_freq == 0:
+            print('saving the latest model (epoch %d, total_steps %d)' %
+                  (epoch, total_steps))
+            model.save(epoch, total_steps)
+
+    if epoch % opt.save_epoch_freq == 0:
+        print('saving the model at the end of epoch %d, iters %d' %
+              (epoch, total_steps))
+        model.save(epoch, total_steps)
+
+    print('End of epoch %d / %d \t Time Taken: %d sec' %
+          (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
