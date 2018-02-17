5a12fb2cb52b47b7f1e8b3f43ec5195d331299bc
林明旺 <linmingwang@linmingwangdeMacBook.local>
2018-02-17 18:58:01 +0800

    debug

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -116,6 +116,7 @@ class KerasCycleGAN:
         print('-----beta1-----', self.opt.beta1)
         print('-----weights_D-----', weights_D, weights_G)
         print('-----lr-----', self.opt.lr)
+        print('-----loss_D-----', loss_D)
         training_updates = Adam(lr=self.opt.lr, beta_1=self.opt.beta1).get_updates(weights_D, [], loss_D)
         D_backward = K.function([real_A, real_B], [loss_DA / 2, loss_DB / 2], training_updates)
 
