6e232dcf90f7244391eb3daba0af0fbed62c635b
林明旺 <linmingwang@linmingwangdeMacBook.local>
2018-02-17 19:13:25 +0800

    debug

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -53,6 +53,7 @@ class KerasCycleGAN:
         return result
 
     def loss_function_lsgan(self, output, target):
+        print('output - target', (output - target).shape, (K.square(output - target)).shape)
         result = K.mean(K.abs(K.square(output - target)))
         return result
 
