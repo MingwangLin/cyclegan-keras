4673005080eb93f4c6865d69bac51f7a1c1ad0f9
linmingwang <aacheve0@gmail.com>
2018-02-25 12:12:05 +0800

    update networks.py

--- models/networks.py
+++ models/networks.py
@@ -1,8 +1,7 @@
 import keras.backend as K
 from keras.models import Sequential, Model
 from keras.layers import Conv2D, ZeroPadding2D, BatchNormalization, Input, Dropout
-from keras.layers import UpSampling2D, Conv2DTranspose, Reshape, Activation, Cropping2D, Flatten
-from keras.layers import add
+from keras.layers import UpSampling2D, Conv2DTranspose, Reshape, Activation, Cropping2D, Flatten, Add
 from keras.layers.advanced_activations import LeakyReLU
 from keras.activations import relu
 from keras.initializers import RandomNormal
@@ -18,15 +17,15 @@ def batchnorm():
                               gamma_initializer=RandomNormal(1., 0.02))
 
 
-def conv_block(x, nf, size, stride=(2, 2), use_norm_layer=True, use_norm_instance=False,
-               use_activation_function=True, use_leaky_relu=False):
-    x = conv2d(nf, (size, size), strides=stride)(x)
-    if use_norm_layer:
+def conv_block(x, filters, size, stride=(2, 2), has_norm_layer=True, use_norm_instance=False,
+               has_activation_layer=True, use_leaky_relu=False, padding='same'):
+    x = conv2d(filters, (size, size), strides=stride, padding=padding)(x)
+    if has_norm_layer:
         if not use_norm_instance:
-            x = batchnorm()(x, training=1)
+            x = batchnorm()(x)
         else:
             x = InstanceNormalization(axis=1)(x)
-    if use_activation_function:
+    if has_activation_layer:
         if not use_leaky_relu:
             x = Activation('relu')(x)
         else:
@@ -34,27 +33,24 @@ def conv_block(x, nf, size, stride=(2, 2), use_norm_layer=True, use_norm_instanc
     return x
 
 
-def res_block(ip, nf=256):
-    x = ZeroPadding2D(padding=(1, 1))(ip)
-    x = conv_block(x, nf, 3, (1, 1))
-    Dropout(0.5)
-    x = ZeroPadding2D(padding=(1, 1))(x)
-    x = conv_block(x, nf, 3, (1, 1), use_activation_function=False)
-    return add([x, ip])
+def res_block(ip, filters=256):
+    x = conv_block(ip, filters, 3, (1, 1))
+    x = Dropout(0.5)(x)
+    x = conv_block(x, filters, 3, (1, 1), has_activation_layer=False)
+    return Add()([x, ip])
 
 
-def up_block(x, nf, size, use_conv_transpose=False, use_norm_instance=False):
+def up_block(x, filters, size, use_conv_transpose=False, use_norm_instance=False):
     if use_conv_transpose:
-        x = Conv2DTranspose(nf, kernel_size=size, strides=2, padding='same',
+        x = Conv2DTranspose(filters, kernel_size=size, strides=2, padding='same',
                             use_bias=True if use_norm_instance else False,
                             kernel_initializer=RandomNormal(0, 0.02))(x)
+        x = batchnorm()(x)
+        x = Activation('relu')(x)
+
     else:
         x = UpSampling2D()(x)
-        x = ZeroPadding2D(padding=(1, 1))(x)
-        x = conv2d(nf, (size, size), strides=(1, 1))(x)
-
-    x = batchnorm()(x, training=1)
-    x = Activation('relu')(x)
+        x = conv_block(x, filters, size, (1, 1))
 
     return x
 
@@ -87,18 +83,19 @@ def n_layer_discriminator(img_size=256, input_nc=3, ndf=64, hidden_layers=2):
         ndf: filters of the first layer
     """
     inputs = Input(shape=(img_size, img_size, input_nc))
-    print('inputs', inputs.shape)
     x = inputs
+
     x = ZeroPadding2D(padding=(1, 1))(x)
-    x = conv_block(x, ndf, 4, use_norm_layer=False, use_leaky_relu=True)
+    x = conv_block(x, ndf, 4, use_norm_layer=False, use_leaky_relu=True, padding='valid')
+
     x = ZeroPadding2D(padding=(1, 1))(x)
     for i in range(1, hidden_layers + 1):
         nf = 2 ** i * ndf
-        x = conv_block(x, nf, 4, use_leaky_relu=True)
+        x = conv_block(x, nf, 4, use_leaky_relu=True, padding='valid')
         x = ZeroPadding2D(padding=(1, 1))(x)
+
     x = conv2d(1, (4, 4), activation='sigmoid', strides=(1, 1))(x)
     outputs = x
-    print('outputs', outputs.shape)
+
+    return Model(inputs=[inputs], outputs=outputs)
     return Model(inputs=inputs, outputs=outputs)
-
-
